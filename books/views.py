47dc783a37c0d7f7e05e98419fd891c232bec95b
Stepan Greenberg <stepan.greenberg@ya.ru>
2022-07-22 14:33:35 +0300

    test not passed

--- books/views.py
+++ books/views.py
@@ -1,14 +1,25 @@
 from .models import Book
-from django.views import generic
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
 
-class BookListView(generic.ListView):
+
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = "books/book_list.html"
     context_object_name = "book_list"
+    login_url = "account_login"
 
 
-
-class BookDetailView(generic.DetailView):
+class BookDetailView(
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+    DetailView,
+):
     model = Book
     template_name = "books/book_detail.html"
     context_object_name = "book"
+    login_url = "account_login"
+    permission_required = "books.special_status"
